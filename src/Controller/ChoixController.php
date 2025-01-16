64d4d08fb661b92222a0d59fa40728a2cc7959e5
unknown <cherrak.imeno@gmail.com>
2025-01-16 11:04:02 +0100

    correction de l'ajout des choix

--- src/Controller/ChoixController.php
+++ src/Controller/ChoixController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Choix;
 use App\Form\ChoixType;
 use App\Repository\ChoixRepository;
+use App\Repository\ScenarioRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/choix')]
 final class ChoixController extends AbstractController
